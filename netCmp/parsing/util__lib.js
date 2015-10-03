b048c14aed33e90736d746b0788b6d9fa2da94f0
Eduard Sedakov <esedakov@msn.com>
2015-10-03 11:26:59 -0700

    (1) added coding for generating hashing string from object (see uitl__sha256.js), (2) added a wrapper for generating hashing string (see util__lib.js), (3) implemented value class that suppose to represent constant value

--- netCmp/parsing/util__lib.js
+++ netCmp/parsing/util__lib.js
@@ -3,10 +3,14 @@
 	Date:		2015-10-02
 	Description:	variety of auxilary functions that facilitate work of parser
 	Used by:	(any class)
-	Dependencies:	(none)
+	Dependencies:	util__sha256.js
 **/
 
 //==========inheritance==========
+//inherite this class (referred by Object) from specified parent class
+//input(s):
+//	parentClass: (Object) => class to inherit from
+//output(s): (none)
 Object.prototype.inheriteFrom = function(parentClass) {
 	//initialize array of arguments passed used by parent constructor
 	var arrParClsArgs = [];
@@ -19,4 +23,14 @@ Object.prototype.inheriteFrom = function(parentClass) {
 	parentClass.apply(this, arrParClsArgs);
 	//inherite from parent class
 	this.prototype = Object.create(parentClass.prototype);
+};
+
+//==========hashing==========
+//generate hashing string for specified object
+//input(s):
+//	obj: (Object) => object (of any type) that needs to be hashed
+//output(s):
+//	(string) => hash value
+Object.prototype.hashCode = function() {
+	Sha256.hash(JSON.stringify(this));
 };
