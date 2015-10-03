0466ea504bb8106cb0693677f909d925d2c426dc
Eduard Sedakov <esedakov@msn.com>
2015-10-02 23:21:47 -0700

    corrected comments (copied from file -- 'util__type__entity.js')

--- netCmp/parsing/type__function.js
+++ netCmp/parsing/type__function.js
@@ -7,13 +7,13 @@
 **/
 
 var FUNCTION_TYPE = {
-	CTOR: {type: 1, name: "constructor"},		//obj__argument.js
-	ADD: {type: 2, name: "add"},			//obj__block.js
-	SUB: {type: 3, name: "subtract"},		//obj__scope.js
-	MUL: {type: 4, name: "multiply"},		//obj__command.js
-	DIV: {type: 5, name: "divide"},			//obj__symbol.js
-	TO_STR: {type: 6, name: "to string"},		//obj__type.js
-	IS_EQ: {type: 7, name: "is equal"},		//obj__value.js
-	CLONE: {type: 8, name: "clone object"},		//util__result.js
-	CUSTOM: {type: 9, name: "user defined"}		//itself
+	CTOR: {type: 1, name: "constructor"},		//constructor
+	ADD: {type: 2, name: "add"},			//operator '+'
+	SUB: {type: 3, name: "subtract"},		//operator '-'
+	MUL: {type: 4, name: "multiply"},		//operator '*'
+	DIV: {type: 5, name: "divide"},			//operator '/'
+	TO_STR: {type: 6, name: "to string"},		//convert object to string
+	IS_EQ: {type: 7, name: "is equal"},		//are two objects equal to each other
+	CLONE: {type: 8, name: "clone object"},		//create copy from given instance
+	CUSTOM: {type: 9, name: "user defined"}		//any other function
 };
---
+++ netCmp/parsing/type__function.js~
@@ -0,0 +1,19 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2015-10-02
+	Description:	types of functions/operators
+	Used by:	functinoid, type
+	Dependencies:	(none)
+**/
+
+var FUNCTION_TYPE = {
+	CTOR: {type: 1, name: "constructor"},		//obj__argument.js
+	ADD: {type: 2, name: "add"},			//obj__block.js
+	SUB: {type: 3, name: "subtract"},		//obj__scope.js
+	MUL: {type: 4, name: "multiply"},		//obj__command.js
+	DIV: {type: 5, name: "divide"},			//obj__symbol.js
+	TO_STR: {type: 6, name: "to string"},		//obj__type.js
+	IS_EQ: {type: 7, name: "is equal"},		//obj__value.js
+	CLONE: {type: 8, name: "clone object"},		//util__result.js
+	CUSTOM: {type: 9, name: "user defined"}		//itself
+};
