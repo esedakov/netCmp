baab3e71d9fc26bc98dcf82f2977ecaf328ebff6
Eduard Sedakov <esedakov@msn.com>
2015-10-02 21:00:48 -0700

    implemented result object, that will be used to represent returned value by parsing functions

--- netCmp/parsing/util__result.js
+++ netCmp/parsing/util__result.js
@@ -3,12 +3,20 @@
 	Date:		2015-10-01
 	Description:	result returned by every parsing routine
 	Used by:	(all parsing routines)
-	Dependencies:	command, block, scope, symbol, function, type
+	Dependencies:	command, block, scope, symbol, function, type, res_ent_type
 **/
 
 //object is returned by parsing functions to determine whether function execute successfully,
 //	and to transfer some information back to the caller (from callee)
 //input(s):
 //	success: (boolean) => did parsing function execute successfully?
-//	//
+//	results: (Array<hash<res_ent_type, object>>) => resulted output
 //output(s): (none)
+function Result(success, results) {
+	this.success = success;	//success flag
+	this.results = results;	//array of hash-maps, where each should contain one key-value pair
+	//e.g. [{command, obj1},{block, obj2},{command, obj3},{scope, obj4},{scope, obj5}, ...]
+};
+
+//create fast access failed result instance
+var FAILED_RESULT = new Result(false, []);
