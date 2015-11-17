aa50fca1644d017b657cc6835fe1e7e2707ce30d
Eduard Sedakov <esedakov@msn.com>
2015-11-16 22:24:25 -0800

    periodic update

--- netCmp/parsing/util__vis.js
+++ netCmp/parsing/util__vis.js
@@ -44,13 +44,140 @@ function viz(id, width, height){
 		command: [],	//same
 		value: []		//same
 	};
+	//collection of functions drawing commands (each has specific number of arguments)
+	this.cmdDrawFuncs = {};	//key: (int) => number of args, value: (function) draw cmd
 };
 
+//create a new prototype for drawing command with specified number of args
+//input(s):
+//	numArgs: (integer) => number of arguments
+//output(s):
+//	(object) => JointJS prototype for drawing command
+viz.prototype.createDrawCmdFunc = function(numArgs){
+	//create markup for command prototype
+	var cmdMarkUp = '<g class="rotatable">' + 
+						'<g class="scalable">' +
+							'<rect/>' + 
+						'</g>' + 
+						'<text class="o_CmdId">' +
+							'<tspan class="i_CmdId"></tspan>' +
+						'</text>' +
+						'<text class="o_CmdTy">' +
+							'<tspan class="i_CmdTy"></tspan>' +
+						'</text>';
+	//create set of command attributes
+	var cmdAttrs = {
+		rect: { 'follow-scale': true, width: 100, height: 100, fill: 'none' },
+		'.i_CmdId': { 'font-size': 23, fill: '#ff7700', 'stroke': '#ff7700' },
+		'.i_CmdTy': { 'font-size': 23, 'font-weight': 'bold', fill: '#000000', 
+						'stroke': '#000000' 
+		}
+	};
+	//create variable number of arguments
+	for( var i = 0; i < numArgs; i++ ){
+		//create argument index
+		var argIdx = i + 1;
+		//append new argument markup
+		cmdMarkUp = cmdMarkUp + 
+					'<text class="o_Arg' + argIdx + '">' +
+						'<tspan class="i_Arg' + argIdx + '"></tspan>' +
+					'</text>';
+		//add new command attribute
+		cmdAttrs['.i_Arg' + argIdx] = {
+			'font-size': 23, fill: '#00ff00', stroke: '#00ff00'
+		};
+	}
+	//finalize markup
+	cmdMarkUp = cmdMarkUp + '</g>',
+	//create command shape
+	joint.shapes['command_' + numArgs] = joint.shapes.basic.Generic.extend({
+
+		//specify command markup
+		markup: cmdMarkUp,
+
+		//setup custom fields for command JointJS shape
+		defaults: joint.util.deepSupplement({
+
+			type: 'command',					//prototype name
+			attrs: cmdAttrs,					//attributes
+			size: { width: 160, height: 35 }	//dimensions
+		}, joint.shapes.basic.Generic.prototype.defaults)
+	});	//end command shape
+	//return resulting command
+	return joint.shapes['command_' + numArgs];
+};	//end function 'createDrawCmdFunc'
+
+//create block shape
+joint.shapes.block = joint.shapes.basic.Generic.extend({
+
+    markup: '<g class="rotatable">' + 
+    			'<g class="scalable">' +
+    				'<rect/>' + 
+    			'</g>' + 
+    			'<text class="o_BlkName">' +
+    				'<tspan class="i_BlkName"></tspan>' +
+    			'</text>' +
+    			'<rect class="minBtn"></rect>' +
+    			'<path class="blkSep"></path>' +
+    		'</g>',
+
+    defaults: joint.util.deepSupplement({
+
+        type: 'block',
+        attrs: {
+            rect: { 'follow-scale': true, width: 300, height: 300, fill: '#440044', opacity: 0.5, rx: 15, ry: 15 },
+            '.i_BlkName': { 'font-size': 23, 'stroke': '#ffffff' },
+            '.minBtn': { width: '15', height: '7', fill: 'red', stroke: 'green', 'stroke-width': 2, rx: 0, ry: 0 },
+            '.blkSep': { 'stroke': '#ff00aa', 'stroke-width': 2, d:'M 0 40 L 300 40' },
+        },
+        size: { width: 300, height: 300 }
+    }, joint.shapes.basic.Generic.prototype.defaults)
+});
+
 //process CFG (control flow graph) and update drawing stack
-//input(s): (none)
+//input(s):
+//	ent: any parser entity
 //output(s): (none)
-viz.prototype.process = function(){
-	//TODO
+viz.prototype.process = function(ent){
+	//make sure that this is a parser entity
+	if( typeof ent !== "object" || ('getTypeName' in ent) == false ){
+		//this is not a parser entity, quit
+		return;
+	}
+	//determine type of parser entity we are dealing with
+	switch(ent.getTypeName().value){
+		case RES_ENT_TYPE.SCOPE.value:
+			//traverse thru child scopes
+			traverse(ent._children);
+			//traverse thru child blocks
+			traverse(ent._blks);
+			break;
+		case RES_ENT_TYPE.BLOCK.value:
+			//traverse thru set of commands
+			traverse(ent._cmds);
+			break;
+		case RES_ENT_TYPE.COMMAND.value:
+			//traverse thru set of arguments of command
+			traverse(ent._args);
+			break;
+		default:
+			//other types (even though could be parsing entities) are not needed, skip
+			break;
+	}
+};
+
+//traverse thru collection of underlying entities
+viz.prototype.traverseThruCollection = function(coll){
+	//iterate thru elements of collection
+	$.each(
+		coll,
+		function(key, value){
+			//ensure that this entry is an object
+			if( typeof value == "object" ){
+				process(value);
+			}	//end if ensure entity is object
+		}	//end iterating function
+	)	//end traversing thru collection
 };
 
 //draw a rectangle using JointJS
