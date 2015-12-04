d549445188c552324dfb03a20dc3e1cd17fdfbc2
Eduard Sedakov <esedakov@msn.com>
2015-12-03 21:15:20 -0800

    next and current functions implemented (not tested) for advancing to the next token and retrieving the current token

--- netCmp/parsing/p__general.js
+++ netCmp/parsing/p__general.js
@@ -22,14 +22,68 @@ function parser(code){
 	var l = new lexer();
 	//process given file into a set of tokens
 	this._tokens = l.process(code);
+	//make sure that set of resulting tokens is not empty
+	if( this._tokens.length == 0 ){
+		throw new Error("345367576445");
+	}
 	//initialize parsing variables
 	this._curTokenIdx = 0;	//current token index
-	this._curLine = 0;		//current code line (for error reporting) number
+	this._curLineIdx = 0;		//current code line (for error reporting) number
 	this._curLineToken = 0;	//currently analyzed token index on the current line
 	this._gScp = null;		//global scope
 	//first create program with global scope
 	var prog = new program();
 	//setup global scope
 	this._gScp = prog.getGlobalScope();
+	//make sure that global scope is set correctly
+	if( this._gScp == null || this._gScp._owner !== null ){
+		throw new Error("12837862728");
+	}
 	//TODO: perform initialization of all types
-};
+};	//end constructor 'parser'
+
+//get current token
+//input(s): (none)
+//output(s):
+//	(token) => current token OR if index of current token is not valid, then the last
+//			available token in lexed code
+parser.prototype.current = function(){
+	//if current token is not valid
+	if( this._curTokenIdx > 0 && this._curTokenIdx < this._tokens.length ){
+		//make sure that set of tokens is not empty
+		if( this._tokens.length == 0 ){
+			throw new Error("8784734678638");
+		}
+		//reset to the last available token
+		this._curTokenIdx = this._tokens.length - 1;
+	}	//end if current token is not valid
+	//access and return current token
+	return this._tokens[this._curTokenIdx];
+};	//end function 'current' to access current token
+
+//advance to next token and update any parsing variables
+//input(s): (none)
+//output(s):
+//	(boolean) => {true} if successfully advanced to the next token, {false} otherwise
+parser.prototype.next = function(){
+	//advance to the next token
+	this._curTokenIdx++;
+	//next token on the current line
+	this._curLineToken++;
+	//loop while current token is a new line
+	while( this.current().type.value == TOKEN_TYPE.NEWLINE.value ){
+		//skip to next token
+		this._curTokenIdx++;
+		//increase current line index
+		this._curLineIdx++;
+		//reset index of token on the current line
+		this._curLineToken = 0;
+		//check that we have not exceeded the token set
+		if( this._curTokenIdx >= this._tokens.length ){
+			//then, there are no more tokens to process
+			return false;
+		}
+	}
+	//if reached this line, then successfully, advanced to next token
+	return true;
+};	//end function 'next' to advance to next token
