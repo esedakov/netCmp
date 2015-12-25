c0d862a73473c77d2ec07391042ef873b5cc6727
Eduard Sedakov <esedakov@msn.com>
2015-12-25 13:48:08 -0800

    type files for identifying types of operations (AND/OR) and node types (ROOT/NON_TERMINAL/TERMINAL)

---
+++ netCmp/parsing/type__log_node.js
@@ -0,0 +1,13 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2015-12-25
+	Description:	types of logical node
+	Used by:	logical tree
+	Dependencies:	(none)
+**/
+
+var LOG_NODE_TYPE = {
+	ROOT: {value: 1, name: "ROOT"},
+	NON_TERMINAL: {value: 2, name: "NON_TERMINAL"},
+	TERMINAL: {value: 3, name: "TERMINAL"}
+};
---
+++ netCmp/parsing/type__log_op.js
@@ -0,0 +1,12 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2015-12-25
+	Description:	types of logical operations
+	Used by:		logical tree
+	Dependencies:	(none)
+**/
+
+var LOGIC_OP = {
+	AND: {value: 1, name: "and"},
+	OR: {value: 2, name: "or"}
+};
