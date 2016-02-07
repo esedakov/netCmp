d10df415f4293bebd1094a4cea7a77ab8a8c2919
Eduard Sedakov <esedakov@msn.com>
2016-02-07 20:08:50 +0400

    new method for interpreter class that is used to process command in CFG -- as of right now it attempts (not tested) to process only a small limited subset of command types, specifically - NOP, NULL, PUSH, ADDA, LOAD. The case for the POP is left for future, when CALL will be implemented.

--- netCmp/interpreter/interpreter.js
+++ netCmp/interpreter/interpreter.js
@@ -47,7 +47,9 @@ function interpreter(code){
 	if( mainFunc._args.length != 0 ){
 		throw new Error("runtime error: MAIN function cannot have any arguments");
 	}
-	//run MAIN function
+	//load variables for this frame
+	this._curFrame.loadVariables();
+	//run user's program, starting from the MAIN function
 	this.run(this._curFrame);
 };	//end constructor for interpreter
 
@@ -55,6 +57,109 @@ function interpreter(code){
 //input(s):
 //	f: (frame) => current frame
 //output(s): (none)
-interpreter.prototype.run = function(){
-	//
+interpreter.prototype.run = function(f){
+	//loop to process commands in this frame
+	do {
+		//get currently executed position in the frame
+		var curPos = f._current;
+		//get currenty executed command
+		var cmd = curPos._cmd;
+		//initialize temporary stack of function arguments
+		var funcArgStk = [];
+		//redirections (i.e. usage of ADDA and LOAD command pair)
+		var redirectCmdMapToEnt = {}; //command{ADDA or LOAD}._id => entity
+		//temporary for storing next position to execute
+		var nextPos = null;
+		//depending on the type of current command
+		switch(cmd._type.value){
+			case COMMAND_TYPE.NOP.value:
+				//do nothing
+			break;
+			case COMMAND_TYPE.NULL.value:
+				//loop thru symbols associated with this command
+				for( tmpSymbId in cmd._defChain ){
+					//make sure that this symbol has associated entity already defined
+					if( !(tmpSymbId in f._symbsToVars) ){
+						//error
+						throw new Error("runtime error: 4738592375897");
+					}	//end if symbol is already defined
+					//get entity for this symbol
+					var tmpEnt = f._symbsToVars[tmpSymbId];
+					//add entity for this command
+					f._cmdsToVars[cmd._id] = tmpEnt;
+				}	//end loop thru associated symbols
+			break;
+			case COMMAND_TYPE.PUSH.value:
+				//initialize variable that stores entity for argument command
+				var tmpArgEnt = null;
+				//if argument command has at least one entity
+				if( cmd._args.length > 0 && cmd._args[0] in f._cmdsToVars ){
+					//set argument command
+					tmpArgEnt = f._cmdsToVars[cmd._args[0]._id];
+					//store value inside argument stack
+					funcArgStk.push(tmpArgEnt._value);
+				} else {
+					throw new Error("runtime error: 9835973857985");
+				}	//end if argument command has at least one entity
+			break;
+			case COMMAND_TYPE.POP.value:
+				//TODO
+			break;
+			case COMMAND_TYPE.LOAD.value:
+				//get its only argument (ADDA command)
+				var tmpAddaCmdId = cmd._args[0]._id;
+				//if there is an entity for ADDA command
+				if( tmpAddaCmdId in f._cmdsToVars ){
+					//add entry to redirection map
+					redirectCmdMapToEnt[cmd._id] = f._cmdsToVars[tmpAddaCmdId];
+				} else {
+					//error
+					throw new Error("runtime error: 3947284731847149817");
+				}	//end if there is an entity for ADDA command
+			break;
+			case COMMAND_TYPE.ADDA.value:
+				//get command of left side of access operator ('.')
+				var tmpLeftSideCmd = cmd._args[0];
+				//get entity for left side's command
+				var tmpLeftSideEnt = f._cmdsToVars[tmpLeftSideCmd._id];
+				//get command or functinoid representing right side
+				var tmpRightSideRef = cmd._args[1];
+				//get third (optional) argument that is used for non-method field
+				//	to represent symbol for the right side
+				var tmpRightSideSymb = cmd._args[2];
+				//store value inside the map '_cmdsToVars', so that LOAD or STORE
+				//	command could use field's of method's reference value:
+				//	1. for data field => entity OR content
+				//	2. for method field => functinoid
+				//if this is a method field (i.e. third argument - symbol is null)
+				var tmpAddaVal = null;
+				if( tmpRightSideSymb == null ){
+					//store functinoid for ADDA's value
+					tmpAddaVal = tmpRightSideRef;	//functinoid reference
+					//also store left side's entity for this ADDA command
+					redirectCmdMapToEnt[cmd._id] = tmpLeftSideEnt;
+				} else {	//otherwise, it is a data field
+					//get entity OR a content representing given field
+					tmpAddaVal = tmpLeftSideEnt._fields[tmpRightSideSymb._name];
+					//store extracted entity/content for ADDA command
+					redirectCmdMapToEnt[cmd._id] = tmpAddaVal;
+				}	//end if it is a method field
+				//store right's side value for ADDA command
+				f._cmdsToVars[cmd._id] = tmpAddaVal;
+			break;
+		}	//end switch -- depending on the type of current command
+		//if 'nextPos' is still NULL, then we simply need to move to the
+		//	next consequent command (if there is any)
+		if( nextPos == null ){
+			//try to get next consequent position
+			nextPos = f.getNextPos();
+			//make sure that there is a next position available
+			if( nextPos == null ){
+				//reached the end, so quit
+				return;
+			}	//end if -- make sure there is a next available position 
+		}	//end if move to next consequent position
+		//move to the next command
+		f._current = nextPos;
+	} while (true);	//end loop to process commands in this frame
 };	//end function 'run'
