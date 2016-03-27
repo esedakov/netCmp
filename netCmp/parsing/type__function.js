2fd5cf850930698e122fbbb33cfbb0d98de642f9
Eduard Sedakov <esedakov@msn.com>
2016-03-27 08:36:42 +0300

    decorative changes

--- netCmp/parsing/type__function.js
+++ netCmp/parsing/type__function.js
@@ -14,11 +14,11 @@ var FUNCTION_TYPE = {
 	MUL: {value: 4, name: "*"},					//operator '*'
 	DIV: {value: 5, name: "/"},					//operator '/'
 	MOD: {value: 6, name: "mod"},				//operator 'mod'
-	TO_STR: {value: 7, name: "toString"},		//convert object to string
-	IS_EQ: {value: 8, name: "isEqual"},			//are two objects equal to each other
-	IS_LESS: {value: 9, name: "isLess"},			//are two objects equal to each other
-	IS_GREATER: {value: 10, name: "isGreater"},			//are two objects equal to each other
-	CLONE: {value: 11, name: "cloneObject"},		//create copy from given instance
+	TO_STR: {value: 7, name: "tostring"},		//convert object to string
+	IS_EQ: {value: 8, name: "isequal"},			//are two objects equal to each other
+	IS_LESS: {value: 9, name: "isless"},			//are two objects equal to each other
+	IS_GREATER: {value: 10, name: "isgreater"},			//are two objects equal to each other
+	CLONE: {value: 11, name: "cloneobject"},		//create copy from given instance
 	MAIN: {value: 12, name: "main"},			//main function
 	CUSTOM_CTOR: {value: 11, name: "constructor"}, 	//custom constructor (optional, i.e. only if user creates a constructor method)
 	//array/hashmap methods
@@ -27,7 +27,7 @@ var FUNCTION_TYPE = {
 	INSERT: {value: 16, name: "insert"},		//insert an element inside the container
 	REMOVE: {value: 17, name: "remove"},		//remove an element from the container
 	INDEX: {value: 18, name: "index"},			//get index for specified array element
-	GET_HASH_CODE: {value: 19, name: "getHashCode"},	//get hashcode (key) for given object (value)
+	GET_HASH_CODE: {value: 19, name: "gethashcode"},	//get hashcode (key) for given object (value)
 	//custom function
 	CUSTOM: {value: 20, name: "user_defined"}	//any other function
 };
