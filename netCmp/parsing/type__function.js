ec9d37793a6e76e540117c31c7eb40791cd22b69
Eduard Sedakov <esedakov@msn.com>
2016-04-03 21:38:28 +0300

    include new function types, specifically for tree, such as getMin or getMax, etc...

--- netCmp/parsing/type__function.js
+++ netCmp/parsing/type__function.js
@@ -22,11 +22,16 @@ var FUNCTION_TYPE = {
 	MAIN: {value: 12, name: "main"},			//main function
 	CUSTOM_CTOR: {value: 11, name: "constructor"}, 	//custom constructor (optional, i.e. only if user creates a constructor method)
 	//array/tree methods
-	LENGTH: {value: 14, name: "length"},		//number of elements in the container
-	GET: {value: 15, name: "get"},				//get specific element from the container
-	INSERT: {value: 16, name: "insert"},		//insert an element inside the container
-	REMOVE: {value: 17, name: "remove"},		//remove an element from the container
-	INDEX: {value: 18, name: "index"},			//get index for specified array element
+	LENGTH: {value: 14, name: "length"},		//number of elements in the container (tree or array)
+	GET: {value: 15, name: "get"},				//get specific element from the container (tree or array)
+	INSERT: {value: 16, name: "insert"},		//insert an element inside the container (tree or array)
+	REMOVE: {value: 17, name: "remove"},		//remove an element from the container (tree or array)
+	INDEX: {value: 18, name: "index"},			//get index for specified array element (for array)
+	IS_INSIDE: {value: 19, name: "isinside"},	//is key inside tree (for tree)
+	REMOVE_ALL: {value: 20, name: "removeall"},	//remove all elements from collection (tree or array)
+	GET_MAX: {value: 21, name: "getmax"},		//get maximum key (for tree)
+	GET_MIN: {value: 22, name: "getmin"},		//get minimum key (for tree)
+	NUM_LEVELS: {value: 23, name: "numlevels"},	//get height of tree (for tree)
 	//custom function
 	CUSTOM: {value: 20, name: "user_defined"}	//any other function
 };
