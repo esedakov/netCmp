2c538bf3d6f38ce584f85e3a2ad37b96292e8c44
esedakov <esedakov@msn.com>
2016-09-20 14:33:07 -0700

    create initializer for new object point

---
+++ netCmp/parsing/obj__pointType.js
@@ -0,0 +1,28 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2016-09-21
+	Description:	point type
+	Used by:	{drawing}, {math}
+	Dependencies: (none)
+**/
+
+//function for creating point type
+//input(s):
+//	gScp: (scope) global scope
+//output(s): (none)
+function create__drawingType(gScp){
+	//create dummy type point
+	var tmp_pt_type = new type("point", OBJ_TYPE.POINT, gScp);
+	//create symbol 'this'
+	tmp_pt_type.createField(
+		"this", 							//variable name
+		tmp_pt_type, 						//variable type
+		tmp_pt_type._scope._start			//first block in the type's scope
+	);
+	//create fundamental functions
+	tmp_pt_type.createReqMethods();
+	//reset command library to avoid cases when NULL command that initializes fields
+	//	of one type, also gets to initialize fields from another type, since it is
+	//	found to be a similar NULL command.
+	command.resetCommandLib();
+};
