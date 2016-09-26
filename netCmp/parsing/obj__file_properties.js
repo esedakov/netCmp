b6d728a5e82cb18407f6017c65e0fe9ef0d46d16
esedakov <esedakov@msn.com>
2016-09-25 20:55:06 -0700

    create accessible fields for file properties

--- netCmp/parsing/obj__file_properties.js
+++ netCmp/parsing/obj__file_properties.js
@@ -19,6 +19,48 @@ function create__filePropertiesType(gScp){
 		tmp_pt_type, 						//variable type
 		tmp_pt_type._scope._start			//first block in the type's scope
 	);
+	//create accessible data field for '_created' date (date when file was created)
+	tmp_pt_type.createField(
+		"_created",							//variable name
+		type.__library["datetime"],			//variable type
+		tmp_pt_type._scope._start			//first block in the type's scope
+	);
+	//create accessible data field for '_modified' date (date when file was last modified)
+	tmp_pt_type.createField(
+		"_modified",						//variable name
+		type.__library["datetime"],			//variable type
+		tmp_pt_type._scope._start			//first block in the type's scope
+	);
+	//create accessible data field for '_path' string (location)
+	tmp_pt_type.createField(
+		"_path",							//variable name
+		type.__library["text"],				//variable type
+		tmp_pt_type._scope._start			//first block in the type's scope
+	);
+	//create accessible data field for '_name' string (file name)
+	tmp_pt_type.createField(
+		"_name",							//variable name
+		type.__library["text"],				//variable type
+		tmp_pt_type._scope._start			//first block in the type's scope
+	);
+	//create accessible data field for '_owner' string (owner name)
+	tmp_pt_type.createField(
+		"_owner",							//variable name
+		type.__library["text"],				//variable type
+		tmp_pt_type._scope._start			//first block in the type's scope
+	);
+	//create accessible data field for '_perms' string (file access/change/delete/rename permissions)
+	tmp_pt_type.createField(
+		"_perms",							//variable name
+		type.__library["text"],				//variable type
+		tmp_pt_type._scope._start			//first block in the type's scope
+	);
+	//create accessible data field for '_size' (file size on the server)
+	tmp_pt_type.createField(
+		"_size",							//variable name
+		type.__library["integer"],			//variable type
+		tmp_pt_type._scope._start			//first block in the type's scope
+	);
 	//create fundamental functions
 	tmp_pt_type.createReqMethods();
 	//reset command library to avoid cases when NULL command that initializes fields
