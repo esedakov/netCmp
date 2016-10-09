5e88f40eb554d151ab55c12e7e52e135581e84c8
esedakov <esedakov@msn.com>
2016-10-08 20:53:37 -0700

    test file as of right now -- later it will recieve POST request with parsing data and store it on db

---
+++ netCmp/parsing/getParserData.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+	Developer:	Eduard Sedakov
+	Date:		2016-10-08
+	Description:	process outter (JSON) request
+	Used by:	none
+	Dependencies:	none
+	*/
+	//print request structure
+	error_log(http_build_query($_REQUEST));
+?>
