86b4c40b0c705f7642efed8d076c7828a0c2e70e
esedakov <esedakov@msn.com>
2016-10-17 12:16:06 -0700

    create session structure

---
+++ netCmp/server/code/init/init__session.php
@@ -0,0 +1,20 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016=10-17
+	Description:	initialize constants
+	Used by:		init__request
+	Dependencies:	(none)
+	*/
+
+	//create session
+	session_start();
+		//how to setup google api: https://developers.google.com/+/web/samples/php
+		//google: project id = netcmp-146118
+		//google: project name = netcmp
+		//google: client id:  553877684601-obu1hedstenc0mj0olbpm193e4s0b6bk.apps.googleusercontent.com
+		//google: client secret:   PvILwgxm6lTFqqTkHYR74WIr  
+		//TODO: I have setup API to be used from localhost, but need to be changed to real URL later
+
+	//
+?>
