f88f28f91a74a83773b0c17f9c38cf84e9a14a3d
esedakov <esedakov@msn.com>
2016-10-17 12:11:00 -0700

    check if session does not exists, which means user has not logged in and just accessed this website so, include code files to initialize session, google api, and mysql

--- netCmp/server/code/init/init__request.php
+++ netCmp/server/code/init/init__request.php
@@ -12,4 +12,19 @@
 	//get name of the file
 	$fileName = $_SERVER['SCRIPT_NAME'];
 	//get file url parameters and split it by '&'
-	$urlParamArr = explode('&', $_SERVER['QUERY_STRING']);
+	$urlParamArr = explode('&', $_SERVER['QUERY_STRING']);	$urlParamArr = explode('&', $_SERVER['QUERY_STRING']);
+
+	//check if session exists
+	//	see: http://stackoverflow.com/questions/3538513/detect-if-php-session-exists
+	if( session_id() == '' || !isset($_SESSION) ){
+
+		//initialize session
+		require 'init__session.php';
+
+		//initialize google api
+		require 'init__gapi.php';
+
+		//initialize mysql
+		require 'init__mysql.php';
+
+	}	//end if session exists
