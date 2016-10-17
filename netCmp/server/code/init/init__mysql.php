f36c497eceeb681b2ffad83ef75c94a31d9128d9
esedakov <esedakov@msn.com>
2016-10-17 12:04:42 -0700

    test mysql connection

---
+++ netCmp/server/code/init/init__mysql.php
@@ -0,0 +1,27 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016=10-17
+	Description:	initialize constants
+	Used by:		init__request
+	Dependencies:	(none)
+	------------------------------------
+	Variables:
+		gapi_images		google file id (if it got created)
+		gapi_texts		google file id (if it got created)
+	*/
+
+	//start mysql access
+	$conn = mysql_connect(
+		'localhost', 
+		$_SESSION['consts']['db']['username'],		//username
+		$_SESSION['consts']['db']['password']		//password
+	);
+
+	//if connection error
+	if ($conn->connect_error) {
+
+		//could not connect to database
+	    die("Connection failed: " . $conn->connect_error);
+
+	}	//end if connection error
