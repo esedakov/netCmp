d57c12f41b10ff7d239a29486c5c518337244835
esedakov <esedakov@msn.com>
2016-12-04 19:05:34 -0800

    remove google api code, will use a different approach for storing files. For now, will store them on the server, later will decide if need to port them to a different location

--- netCmp/server/code/init/init__mysql.php
+++ netCmp/server/code/init/init__mysql.php
@@ -22,39 +22,4 @@
 
 	}	//end if connection error
 
-	//init sql statement for updating google api information if needed
-	$sqlUpdGApi = '';
-
-	//if google file for images has been created
-	if( $gapi_images != '' ){
-
-		//create SET statement for updating file id for file 'images'
-		$sqlUpdGApi .= "SET img_file_id = '" . $gapi_images . "' ";
-
-	}	//end if google file for images has been created
-
-	//if google file for texts has been created
-	if( $gapi_texts != '' ){
-
-		//create SET statement for updating file id for file 'texts'
-		$sqlUpdGApi .= ($sqlUpdGApi == '' ? '' : ',') . "SET img_file_id = '" . $gapi_texts . "' ";
-
-	}	//end if google file for texts has been created
-
-	//if need to update table that stores google api information
-	if( $sqlUpdGApi != '' ){
-
-		//complete sql statement
-		$sqlUpdGApi = 'UPDATE netcmp_google_api ' . $sqlUpdGApi;
-
-		//if sql statement did not execute successfully
-		if ($conn->query($sqlUpdGApi) === FALSE) {
-
-			//could not update database
-			die("Could not initialize google api table " . $conn->error);
-
-		}	//end if sql statement did not execute successfully
-
-	}	//end if need to update table that stores google api information
-
 ?>
