27f62da4dc9675284e99385545c0ee75d19c0f77
esedakov <esedakov@msn.com>
2016-12-04 19:27:18 -0800

    remaining js

--- netCmp/server/code/vw__page.php
+++ netCmp/server/code/vw__page.php
@@ -24,11 +24,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<!-- external libraries -->
+		<link rel="stylesheet" type="text/css" href="../external/joint.css">
 		<!-- ES 2016-09-11 (b_debugger): added 2 stylesheets to make viewport DIVs resizable -->
 		<link rel="stylesheet" href="../external/jquery/ui/1.12.0/themes/base/jquery-ui.css">
 		<link rel="stylesheet" href="../external/resources/demos/styles.css">
 		<!-- Bootstrap core CSS -->
 		<link href="../external/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<script src="../external/jquery.min.js"></script>
+		<script src="../external/lodash.min.js"></script>
+		<script src="../external/backbone-min.js"></script>
+		<script src="../external/joint.js"></script>
 		<!-- ES 2016-09-11 (b_debugger): added UI jquery library to make DIV resizable -->
 		<script src="../external/jquery-ui.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
