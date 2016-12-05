f95a390f389dbde625a05d868dea49167d007988
esedakov <esedakov@msn.com>
2016-12-04 19:24:03 -0800

    description

---
+++ netCmp/server/code/vw__page.php
@@ -0,0 +1,12 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016=10-17
+	Description:	show header (user info + about) and tail (company) parts of the html page
+	Used by:		(init)
+	Dependencies:	(none)
+	*/
+
+?>
+?>
+<!-- start html -->
