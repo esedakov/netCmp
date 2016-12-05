65b2e86c9a0b452e94fce36a7ec5341860d7848d
esedakov <esedakov@msn.com>
2016-12-04 19:26:52 -0800

    jquery js and css

--- netCmp/server/code/vw__page.php
+++ netCmp/server/code/vw__page.php
@@ -24,8 +24,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<!-- external libraries -->
+		<!-- ES 2016-09-11 (b_debugger): added 2 stylesheets to make viewport DIVs resizable -->
+		<link rel="stylesheet" href="../external/jquery/ui/1.12.0/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="../external/resources/demos/styles.css">
 		<!-- Bootstrap core CSS -->
 		<link href="../external/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<!-- ES 2016-09-11 (b_debugger): added UI jquery library to make DIV resizable -->
+		<script src="../external/jquery-ui.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="../external/bootstrap/js/bootstrap.min.js"></script>
 		<!-- bootstrap required -->
