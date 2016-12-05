001a73bc5c23dd26db40789e23b8e33861ced193
esedakov <esedakov@msn.com>
2016-12-04 19:18:05 -0800

    create new function for getting user information -- nc__lib__getUser

--- netCmp/server/code/lib/lib__getUserInfo.php
+++ netCmp/server/code/lib/lib__getUserInfo.php
@@ -10,4 +10,21 @@
 	//include functions for DB
 	require 'lib__db.php';
 
+	//get user information, entered during registration
+	//input(s):
+	//	id: user id
+	//output(s):
+	//	Array:
+	//		name -> user name
+	//		email -> user email
+	//		created -> date at which account was created
+	//		modified -> date at which account was last modified
+	//		logo -> file id for logo image
+	//		suspend -> is account suspended
+	function nc__lib__getUser($id){
+
+
+	}	//end function 'nc__lib__getUser'
+
+
 ?>
