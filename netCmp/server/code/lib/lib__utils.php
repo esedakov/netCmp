0ac2e6cfec2d9ad7727365e7fcca8bb146d7c1d2
esedakov <esedakov@msn.com>
2016-12-04 19:21:11 -0800

    create new function for redirecting to another page -- nc__util__redirect

--- netCmp/server/code/lib/lib__utils.php
+++ netCmp/server/code/lib/lib__utils.php
@@ -7,4 +7,14 @@
 	Dependencies:	(none)
 	*/
 
+	//redirect to another page
+	//see: http://stackoverflow.com/questions/353803/redirect-to-specified-url-on-php-script-completion
+	//input(s):
+	//	path: (text) partial path (starting from 'netcmp' folder) to the redirected file
+	//output(s): (none)
+	function nc__util__redirect($path){
+
+
+	}	//end function 'nc__util__redirect'
+
 ?>
