20f8e28c870219581adb3f8900fefd9e156487a3
esedakov <esedakov@msn.com>
2016-12-06 19:59:05 -0800

    add buttons, which are organized vertically on the side to manipulate with various aspects of user and file information

---
+++ netCmp/server/code/vw__toolbar.php
@@ -0,0 +1,174 @@
+	<div class="col-xs-1 col-md-1" style="height:100%;">
+		<!-- see: http://stackoverflow.com/questions/18192114/how-to-use-vertical-align-in-bootstrap -->
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Variables"
+			>
+				<span 
+					class="glyphicon glyphicon-tower" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<!--<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Project"
+			>
+				<span 
+					class="glyphicon glyphicon-tasks" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>-->
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Files & Folders"
+			>
+				<span 
+					class="glyphicon glyphicon-folder-open" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Users"
+			>
+				<span 
+					class="glyphicon glyphicon-user" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="User Interface"
+			>
+				<span 
+					class="glyphicon glyphicon-compressed" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Messages"
+			>
+				<span 
+					class="glyphicon glyphicon-comment" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<hr class="featurette-divider">
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Application"
+			>
+				<span 
+					class="glyphicon glyphicon-font" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Debugger"
+			>
+				<span 
+					class="glyphicon glyphicon-record" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Code"
+			>
+				<span 
+					class="glyphicon glyphicon-pencil" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<hr class="featurette-divider">
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Run"
+			>
+				<span 
+					class="glyphicon glyphicon-play" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Stop"
+			>
+				<span 
+					class="glyphicon glyphicon-stop" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Restart"
+			>
+				<span 
+					class="glyphicon glyphicon-repeat" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+		<div class="row vertBarIcon" style="height:5%">
+			<div 
+				class="col-xs-12 col-md-12"
+				data-toggle="tooltip"
+				data-placement="right"
+				title="Pause"
+			>
+				<span 
+					class="glyphicon glyphicon-pause" 
+					aria-hidden="true"
+				></span>
+			</div>
+		</div>
+	</div>
