1898f09eae481c75ee0255fa7cd63cc16273f525
esedakov <esedakov@msn.com>
2016-12-06 19:56:35 -0800

    include styles for formatting the code, which is entered by the user on the page to be compiled

--- netCmp/server/code/vw__page.php
+++ netCmp/server/code/vw__page.php
@@ -76,6 +76,77 @@
 			.vertBarIcon:hover {
 				color: purple;
 			}
+			<?php //styles for vw__codeview.php, i.e. style the typed in code ?>
+			.nc-lang-keyword {
+				font-weight: bold;
+				color: green;
+			}
+			.nc-type {
+				color: purple;
+				text-decoration: underline;
+			}
+			.nc-identifier {
+				font-style: italic;
+				color: blue;
+			}
+			.nc-constant-value {
+				background-color: lightgray;
+				color: red;
+			}
+			.nc-tab {
+				margin-left: 2em;
+			}
+			.nc-editor-current-line {
+				background-color: #f2dede;
+			}
+			.nc-line {
+				display: block;
+				min-height: 2ex;
+			}
+			.nc-console {
+				background-color: #bb8181;
+				width: 1em;
+				height: 2ex;
+				display: inline-block;
+				top: 2px;
+				opacity: 0.5;
+			}
+			.nc-emphasize-identifiers {
+				font-weight: bold;
+				font-size: 3ex;
+			}
+			.nc-current-word {
+				/* nothing */
+			}
+			.nc-current-letter {
+				background-color: aqua;
+				border-right-color: red;
+				border-right-style: solid;
+				border-right-width: 2px;
+			}
+			.nc-comment {
+				color: #a3cab6;
+				/* subsequent styles override all other that are defined for SPANs */
+				font-weight: normal;
+				text-decoration: initial;
+				font-style: initial;
+				background-color: initial;
+				font-size: initial;
+			}
+			.nc-comment-start {
+				/* nothing */
+			}
+			.nc-comment-end {
+				/* nothing */
+			}
+			.nc-comment-one-line {
+				/* nothing */
+			}
+			.nc-clicked-element {
+				border-width: 2px;
+				border-color: red;
+				border-style: solid;
+			}
 		</style>
 
 	</head>	<!-- end head -->
