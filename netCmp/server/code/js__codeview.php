2b98ae69c428a0854c153b49b8bd05c2175b3ab6
esedakov <esedakov@msn.com>
2016-12-08 19:47:17 -0800

    split line on word-by-word basis, using syntax and white spaces as separators (keep those separators in)

--- netCmp/server/code/js__codeview.php
+++ netCmp/server/code/js__codeview.php
@@ -1,2 +1,10 @@
 	//process tokens for the specified line and output resulting HTML
 	function processLine(lineNum, doAdjustTabInfo){
+		//remove whitespaces from beginning and end string and split text word-by-word 
+		var words = g_code[lineNum]	//$(".nc-editor-current-line")
+			.replace(/[\s]+/g, " ")
+			.trim()
+			//TODO: add other symbols, such as arithmetic symbols
+			//	but keep in mind that '/*' can be interpreted as '/' and '*', separately
+			.split(/(,|=|:|;|\(|\)|\{|\}|\s|\<|\>|\/\/|\/\*|\*\/)/g);
+		//create container for processed text
