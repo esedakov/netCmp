e868673704e95ef10ccc4810ea0cba7a6faaec7b
esedakov <esedakov@msn.com>
2016-12-08 19:53:00 -0800

    perform setup before identifying words by their content: - skip white and empty spaces - identify syntax - wrap each letter in its own container (so that we can navigate letter-by-letter) - start word container (span)

--- netCmp/server/code/js__codeview.php
+++ netCmp/server/code/js__codeview.php
@@ -16,17 +16,35 @@
 		//counter: count letters in the processed line
 		var tmpLetterCounter = 0;
 		//for each resulted word
-		$.each(words, function(index, value){		});			//flag: is this a current word
+		$.each(words, function(index, value){
+			//ignore empty string and white space
+			if( value == "" ){ //|| value == " " ){
+				return;
+			}
+			//flag: is this a current word
 			var tmpIsCurWord =	tmpLetterCounter <= (g_curLetterNum - 1) 
 								&& (tmpLetterCounter + value.length) >= g_curLetterNum;
-			//store current word with each character encapsulated in its SPAN				//check if this is a current word AND current letter
+			//store current word with each character encapsulated in its SPAN
+			var tmpC = "";
+			//split string into array of characters
+			var tmpValCharArr = value.split("");
+			//is it numeric?
+			var isNumeric = $.isNumeric(tmpValCharArr[0]);
+			//loop thru character array to encapsulte current character in a separate SPAN
+			for( var i = 0; i < tmpValCharArr.length; i++ ){
+				//start SPAN
+				tmpC += "<span class='";
+				//check if this is a current word AND current letter
 				if( tmpIsCurWord && (tmpLetterCounter + i) == (g_curLetterNum - 1) ){
 					//add class for the current letter
 					tmpC += "nc-current-letter ";
 				}
 				//end SPAN
 				tmpC += "nc-processed-letter'>" + tmpValCharArr[i] + "</span>";
-			}	//end loop thru character array			//if this is current word
+			}	//end loop thru character array
+			//start the SPAN
+			res += "<span class='";
+			//if this is current word
 			if( tmpIsCurWord ) {
 				//add class for the current word
 				res += "nc-current-word ";
