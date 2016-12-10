8edcedec25dcae0dc03e94d19cac3a3b408670ce
esedakov <esedakov@msn.com>
2016-12-10 14:55:44 -0800

    include libraries for DB and utility functions

--- netCmp/server/code/pr__register.php
+++ netCmp/server/code/pr__register.php
@@ -6,3 +6,9 @@
 	Used by:		(post__login)
 	Dependencies:	(init__request),(lib__db)
 	*/
+
+	//process incoming url parameters
+	require_once './init/init__request.php';
+
+	//include library for DB functions
+	require_once './lib/lib__db.php';
