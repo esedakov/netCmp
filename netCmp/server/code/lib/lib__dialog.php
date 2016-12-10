460461c89f76b9abc3f2f473736ce1cbecea27ec
esedakov <esedakov@msn.com>
2016-12-09 16:16:45 -0800

    create function for starting dialog (jquery style dialog)

--- netCmp/server/code/lib/lib__dialog.php
+++ netCmp/server/code/lib/lib__dialog.php
@@ -5,4 +5,20 @@
 	Description:	library of dialog functions
 	Used by:		(vw__page)
 	Dependencies:	(lib__utils)
-	*/
+	*/	//start dialog
+	//input(s):
+	//	src: (text) page url that should be displayed inside this dialog
+	//	attrs: (array<>) set of attributes, list of possible attributes include:
+	//		- id: (text) unique id for this dialog to referenced by jquery
+	//		- caption: (text) caption, displayed in the dialog's header
+	//		- headCol: (text) header's color
+	//		- headTxtCol: (text) header's text color
+	//		- headTxtSize: (text) header's text size
+	//		- needClose: (boolean) need a close button
+	//		- needMove: (boolean) need dialog to move
+	//		- needRefresh: (boolean) need top parent page to be refreshed upon closing this dialog
+	//output(s):
+	//	(text) => dialog id
+	function nc__dlg__start($attrs){
+
+	}	//end function 'nc__dlg__start'
