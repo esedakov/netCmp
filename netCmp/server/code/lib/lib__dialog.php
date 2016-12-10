56c67b10793aa420a76f2ac855d7b88cb4c52a1e
esedakov <esedakov@msn.com>
2016-12-09 16:17:53 -0800

    perform checks for every possible attribute value that can be specified for dialog, and assign default values if such attributes are not specified

--- netCmp/server/code/lib/lib__dialog.php
+++ netCmp/server/code/lib/lib__dialog.php
@@ -21,4 +21,68 @@
 	//	(text) => dialog id
 	function nc__dlg__start($attrs){
 
+		//check if 'caption' is not defined among attributes
+		if( array_key_exists("caption", $attrs) == false ){
+
+			//define default caption
+			$attrs["caption"] = "dialog";
+
+		}	//end if 'caption' is not defined among attributes
+
+		//check if 'headCol' is not defined among attributes
+		if( array_key_exists("headCol", $attrs) == false ){
+
+			//define default header's color
+			$attrs["headCol"] = "#5cb85c";
+
+		}	//end if 'headCol' is not defined among attributes
+
+		//check if 'headTxtCol' is not defined among attributes
+		if( array_key_exists("headTxtCol", $attrs) == false ){
+
+			//define default header's text color
+			$attrs["headTxtCol"] = "white";
+
+		}	//end if 'headTxtCol' is not defined among attributes
+
+		//check if 'headTxtSize' is not defined among attributes
+		if( array_key_exists("headTxtSize", $attrs) == false ){
+
+			//define default header's text size
+			$attrs["headTxtSize"] = "30px";
+
+		}	//end if 'headTxtSize' is not defined among attributes
+
+		//check if 'needClose' is not defined among attributes
+		if( array_key_exists("needClose", $attrs) == false ){
+
+			//define flag -- need close button
+			$attrs["needClose"] = true;
+
+		}	//end if 'needClose' is not defined among attributes
+
+		//check if 'needMove' is not defined among attributes
+		if( array_key_exists("needMove", $attrs) == false ){
+
+			//define flag -- need dialog to be moveable
+			$attrs["needMove"] = true;
+
+		}	//end if 'needMove' is not defined among attributes
+
+		//check if 'needRefresh' is not defined among attributes
+		if( array_key_exists("needRefresh", $attrs) == false ){
+
+			//define flag -- need top parent window to be refereshed
+			$attrs["needRefresh"] = false;
+
+		}	//end if 'needRefresh' is not defined among attributes
+
+		//check if 'id' is not defined among attributes
+		if( array_key_exists("id", $attrs) == false ){
+
+			//define unique id based on the page name and random number
+			$attrs["id"] = nc__util__getPHPFileName() . "__dlg__" . rand(1000,100000);
+
+		}	//end if 'id' is not defined among attributes
+
 	}	//end function 'nc__dlg__start'
