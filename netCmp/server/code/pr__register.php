1ae5b1d9447ce664c8505fab7981b7882b855f26
esedakov <esedakov@msn.com>
2016-12-10 14:55:16 -0800

    include comments that specifies authoring rights

---
+++ netCmp/server/code/pr__register.php
@@ -0,0 +1,8 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016-12-11
+	Description:	complete registration of a new user
+	Used by:		(post__login)
+	Dependencies:	(init__request),(lib__db)
+	*/
