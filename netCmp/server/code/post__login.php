a55411d8d4888b883e5950674e5edc753e8feae4
esedakov <esedakov@msn.com>
2016-12-10 14:58:40 -0800

    include libraries for DB and uitility functions

--- netCmp/server/code/post__login.php
+++ netCmp/server/code/post__login.php
@@ -6,3 +6,9 @@
 	Used by:		(vw__login)
 	Dependencies:	(lib__db),(lib__utils)
 	*/
+
+	//include library for db function 'nc__db__getDBCon'
+	require_once './lib/lib__db.php';
+
+	//include library for utility functions
+	require_once './lib/lib__utils.php';
