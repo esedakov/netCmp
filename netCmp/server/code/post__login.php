0d41da8376d431818144668fb4104095021610b1
esedakov <esedakov@msn.com>
2016-12-10 14:59:14 -0800

    make sure that required fields have been entered, i.e. username and password

--- netCmp/server/code/post__login.php
+++ netCmp/server/code/post__login.php
@@ -12,3 +12,17 @@
 
 	//include library for utility functions
 	require_once './lib/lib__utils.php';
+
+	//if any required field is not set
+	if(
+		//if user name is not specified
+		nc__util__isFieldSet("nc_user_name") == false ||
+
+		//if password is not specified
+		nc__util__isFieldSet("nc_user_password") == false 
+	) {
+
+		//error -- (TODO) -- user user name or password is not specified
+		nc__util__error("user name or password is not set during login/registration step");
+
+	}	//end if any required fields is not set
