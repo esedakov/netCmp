7adf63caef4e2b896797813cd4f170c174c72ac7
esedakov <esedakov@msn.com>
2016-12-09 16:11:16 -0800

    create fields that are always required (in login and in register); also checkbox to 'remember me'

--- netCmp/server/code/vw__login.php
+++ netCmp/server/code/vw__login.php
@@ -16,6 +16,23 @@
 			<li role="presentation" class="nc-login-toggle-pills"><a href="#">Register</a></li>
 		</ul>
 
+		<hr class="featurette-divider">
+
+		<label for="inputEmail" class="sr-only">Email address</label>
+		<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+
+		
+		<label for="inputPassword" class="sr-only">Password</label>
+		<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+		<hr class="featurette-divider">
+			
+		<div class="checkbox">
+			<label>
+				<input type="checkbox" value="remember-me"> Remember me
+			</label>
+		</div>
+
 	</form>	<!-- /form-signin -->
 
 </div> <!-- /container -->
