ee6fdbbae8b4a90e12c38ef8f887d550a21487bd
esedakov <esedakov@msn.com>
2016-12-10 14:58:24 -0800

    include comments for specifying authoring information

---
+++ netCmp/server/code/post__login.php
@@ -0,0 +1,8 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016-12-11
+	Description:	login and if necessary register new user
+	Used by:		(vw__login)
+	Dependencies:	(lib__db),(lib__utils)
+	*/
