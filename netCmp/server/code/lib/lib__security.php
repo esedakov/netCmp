a67e9cfee4deeaacdb49b102b3a6c6779b991056
esedakov <esedakov@msn.com>
2016-12-17 08:32:42 -0800

    file description with author information

---
+++ netCmp/server/code/lib/lib__security.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016-12-11
+	Description:	library functions for encrypting/decrypting/encoding/decoding
+						see: http://stackoverflow.com/a/30189841
+	Used by:		(everything)
+	Dependencies:	(lib__utils)
+	*/
+
+?>
