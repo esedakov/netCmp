da4ede03eef08978dbc8eecd3b2eded52f2e7289
esedakov <esedakov@msn.com>
2016-12-20 05:03:42 -0800

    invoked via AJAX to update state of the window view: expanded or shrinked (this AJAX call is triggerred by a button on the right-top side of the page, below page header piece) this file simply updates session structure with view state

--- netCmp/server/code/pr__expandview.php
+++ netCmp/server/code/pr__expandview.php
@@ -7,4 +7,18 @@
 	Dependencies:	(none)
 	*/
 
+	//check if POST has 'exp' flag
+	if( array_key_exists('e', $_POST) ){}
+
+		//include library for function 'nc__util__reInitSession'
+		require_once './lib/lib__utils.php';
+
+		//re-initialize session
+		nc__util__reInitSession();
+
+		//record data inside session
+		$_SESSION["view"]['mode'] = $_POST["e"];
+
+	}	//end if POST has 'exp' flag
+
 ?>
