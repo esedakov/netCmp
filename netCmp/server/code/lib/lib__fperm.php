22a54c589accbb08c719911d26a823486d689629
esedakov <esedakov@msn.com>
2016-12-20 05:34:32 -0800

    implement to string operator that would translate integer constant to textual representation

--- netCmp/server/code/lib/lib__fperm.php
+++ netCmp/server/code/lib/lib__fperm.php
@@ -23,6 +23,19 @@
 		//move or rename permission
 		const MOVE = 8;		//1000
 
+		//convert constant values into textual representations
+		//input(s):
+		//	constObj: (NC__ENUM__FPERM:READ/WRITE/DELETE/MOVE) permission constant
+		//output(s):
+		//	(text) textual representation
+		public static function toStr($constObj){
+
+			return  ((($constObj & 1) == 1) ? "READ_" : "") .
+					((($constObj & 2) == 2) ? "WRITE_" : "") .
+					((($constObj & 4) == 4) ? "DEL_" : "") .
+					((($constObj & 8) == 8) ? "MOVE" : "");
+
+		}	//end function toStr
 
 	}	//end of class 'NC__ENUM__FPERM'
 
