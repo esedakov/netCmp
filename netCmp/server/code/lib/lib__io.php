bc907d03edd11ad16255818670badb999dcb4ca6
esedakov <esedakov@msn.com>
2016-12-20 05:19:03 -0800

    specify file description including authoring information

---
+++ netCmp/server/code/lib/lib__io.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016-12-18
+	Description:	utility functions for input/output, i.e. file/folder manipulations
+	Used by:		(code_view), (file_folder_view)
+	Dependencies:	(lib_db), (lib_utils)
+	*/
+
+?>
