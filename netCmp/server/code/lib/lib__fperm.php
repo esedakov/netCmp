473d238d688a0f8e78234a80da11859c1adcf83a
esedakov <esedakov@msn.com>
2016-12-20 05:33:49 -0800

    PHP has no enum, but this abstract class somewhat resembles enum idea, by not allowing to instantiation of the class itself. In the same type, it can serve as a namespace, thus it is better then declaring stand alone constants which would otherwise require long unique names to prevent collissions between each other

--- netCmp/server/code/lib/lib__fperm.php
+++ netCmp/server/code/lib/lib__fperm.php
@@ -7,4 +7,23 @@
 	Dependencies:	(none)
 	*/
 
+	//create an abstract class as a workaround for enumeration
+	//	see: http://stackoverflow.com/a/254543
+	abstract class NC__ENUM__FPERM {
+
+		//read/access permission
+		const READ = 1;		//0001
+
+		//write permission
+		const WRITE = 2;	//0010
+
+		//delete permission
+		const DELETE = 4;	//0100
+
+		//move or rename permission
+		const MOVE = 8;		//1000
+
+
+	}	//end of class 'NC__ENUM__FPERM'
+
 ?>
