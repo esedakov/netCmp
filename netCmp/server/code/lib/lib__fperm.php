14b236e3c0dc235f1b9ff14ad6c693b89d02cbab
esedakov <esedakov@msn.com>
2016-12-20 05:31:23 -0800

    add file description that also will specify authoring information

---
+++ netCmp/server/code/lib/lib__fperm.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016-12-19
+	Description:	file/folder access and modification permissions
+	Used by:		(lib_db), (lib_io)
+	Dependencies:	(none)
+	*/
+
+?>
