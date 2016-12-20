1068536f0ae4eaf6aa1e6beb65d8529bf2380baf
esedakov <esedakov@msn.com>
2016-12-20 05:29:34 -0800

    set all fields as publicly accessible

--- netCmp/server/code/lib/lib__fattr.php
+++ netCmp/server/code/lib/lib__fattr.php
@@ -13,6 +13,29 @@
 	//create class to represent file/folder attributes, which are used for DB updates
 	class nc__class__fattr {
 
+		//modification date
+		//	type: PHP DATETIME
+		public $_date;
+
+		//file/folder permissions
+		//	type: NC__ENUM__FPERM
+		public $_fperm;
+
+		//file/folder name
+		//	type: TEXT
+		public $_name;
+
+		//id of the owner
+		//	type: INT
+		public $_ownerId;
+
+		//parent directory id
+		//	type: INT
+		public $_dirId;
+
+		//flag: is suspended
+		//	type: BOOL
+		public $_isSuspnded;
 
 	}	//end class 'nc__class__fattr'
 
