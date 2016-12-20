6e3938b24399b89dea448f6a9e4dab3ee49b1efe
esedakov <esedakov@msn.com>
2016-12-20 05:17:03 -0800

    specify file description with authoring information

---
+++ netCmp/server/code/lib/lib__json.php
@@ -0,0 +1,11 @@
+<?
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016-12-19
+	Description:	utility functions to manipulate JSON, which will deliver both parsing data
+						and file contents from client to server (primarily via AJAX)
+	Used by:		()
+	Dependencies:	(none)
+	*/
+	
+?>
