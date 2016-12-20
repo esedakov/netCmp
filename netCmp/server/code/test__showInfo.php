a7fce96efb6c74e02b2f1ace2f08cb1926b6f090
esedakov <esedakov@msn.com>
2016-12-20 05:00:30 -0800

    dump only session, but make sure that one exists

--- netCmp/server/code/test__showInfo.php
+++ netCmp/server/code/test__showInfo.php
@@ -1,5 +1,9 @@
 <?php
+	//include library for function 'nc__util__reInitSession'
+	require_once './lib/lib__utils.php';
+	//re-initialize session
+	nc__util__reInitSession();
 	var_dump($_SESSION);
 	echo("\n=================================\n");
-	var_dump($GLOBALS);
+	//var_dump($GLOBALS);
 ?>
