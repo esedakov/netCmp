3d9615f9bdb12fa36ff061581b120ba9e8127bb5
esedakov <esedakov@msn.com>
2016-12-20 05:18:25 -0800

    set information for future function declarations to manipulate JSON, which is recieved from client via AJAX call and will carry not only parsing data, but also file contents (such as images)

--- netCmp/server/code/lib/lib__json.php
+++ netCmp/server/code/lib/lib__json.php
@@ -8,4 +8,28 @@
 	Dependencies:	(none)
 	*/
 	
+	//receive JSON from js client
+	//input(s): (none)
+	//output(s):
+	//	(Array<String> => String/Base64) field name => field values
+	//	Note: $_POST['mydata'] if in JS, json was formed as follows: {mydata: ...}
+	//		see: http://stackoverflow.com/a/10955053
+	//	also, see: http://stackoverflow.com/a/23981045
+
+	//add image to the JSON
+	//input(s):
+	//	json: (Array<String> => String/Base64) json object
+	//	fileName: (text) file name
+	//	cnt: (PHP Image Data) image data
+	//output(s):
+	//	(Array<String> => String/Base64) json object
+	//	see: http://stackoverflow.com/a/13758760
+
+	//extract image object from JSON
+	//input(s):
+	//	json: (Array<String> => String/Base64) json object
+	//	fieldName: (text) field name that referres to the image
+	//output(s):
+	//	(text) => base64 string that represents image
+
 ?>
