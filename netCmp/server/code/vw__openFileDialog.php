25cc7d8e00c03c30fde809f3624ba1a308bbfe86
esedakov <esedakov@msn.com>
2016-12-30 05:18:37 -0800

    include libraries for utils, io, and security (last is not really used, now -- premise was to use it for encrypting file/folder id)

--- netCmp/server/code/vw__openFileDialog.php
+++ netCmp/server/code/vw__openFileDialog.php
@@ -7,4 +7,11 @@
 	Dependencies:	(io)
 	*/
 
+	//include library for function 'nc__util__reInitSession'
+	require_once './lib/lib__utils.php';
+	//include library for function 'nc__io__getIOEntries'
+	require_once './lib/lib__io.php';
+	//include security library for 'nc__secur__encode' and 'nc__secur__encrypt'
+	require_once './lib/lib__security.php';
+
 ?>
