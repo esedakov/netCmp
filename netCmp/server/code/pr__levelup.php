c832559920eeb5ac1d804daeb12467eff4dd76eb
esedakov <esedakov@msn.com>
2016-12-31 13:48:46 -0800

    show file description information along with authoring rights

---
+++ netCmp/server/code/pr__levelup.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+	Developer:		Eduard Sedakov
+	Date:			2016-12-30
+	Description:	try to navigate one level up (open-file browsing)
+	Used by:		(AJAX:vw__openFileDialog.php)
+	Dependencies:	(lib__utils.php)
+	*/
+
+?>
