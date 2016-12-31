5168ed1184bdc65e4c8205f4ed7a83f292b9c845
esedakov <esedakov@msn.com>
2016-12-31 14:02:25 -0800

    establish the only function in this file (at least, right now) -- createContextMenu

--- netCmp/server/code/lib/lib__contextMenu.php
+++ netCmp/server/code/lib/lib__contextMenu.php
@@ -7,4 +7,21 @@
 	Dependencies:	(none)
 	*/
 
+	//create context menu
+	//Inputs:
+	//	menu (array<id:integer, caption:text>) identify elements of context menu and which
+	//		ids are to be returned upon a click event on any of such elements
+	//	onClickHandler (js function) function to trigger when menu item is clicked
+	//		Note: clicked element's id is passed in to this function.
+	//	cls (text) CSS class of HTML objects that will be right-clicked to open this menu
+	//	uniqId (text) context menu unique id
+	//	ignoreCls (text) CSS class to ignore when handling right click
+	//output(s): (none)
+	//	see: http://jsbin.com/iGaHAtu/2/edit?html,css,js,output
+	//	see: http://stackoverflow.com/a/18667012
+	function createContextMenu($menu, $onClickHandler, $cls, $uniqId, $ignoreCls){
+
+
+	}	//end function 'createContextMenu'
+
 ?>
