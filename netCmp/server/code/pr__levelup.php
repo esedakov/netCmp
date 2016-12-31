ed651b8aa3a919f57fcfe9e2ed0ae4d7cadb5974
esedakov <esedakov@msn.com>
2016-12-31 13:49:15 -0800

    include libraries: utils and db also create/request session

--- netCmp/server/code/pr__levelup.php
+++ netCmp/server/code/pr__levelup.php
@@ -7,4 +7,12 @@
 	Dependencies:	(lib__utils.php)
 	*/
 
+	//include library for function 'nc__util__reInitSession'
+	require_once './lib/lib__utils.php';
+
+	//include DB library for function 'nc__db__getIOEntryAttrs'
+	require_once './lib/lib__db.php';
+
+	//re-initialize session
+	nc__util__reInitSession();
 ?>
