1623dbe484604ba7d49a683293d00896ddf75a74
esedakov <esedakov@msn.com>
2016-12-31 13:27:45 -0800

    create context menu item 'nc_cntx_menu_specific' for actual file/folder items and one more 'nc_cntx_menu_general' for area around file/folder items

--- netCmp/server/code/vw__openFileDialog.php
+++ netCmp/server/code/vw__openFileDialog.php
@@ -23,6 +23,45 @@
 	//	TODO: for now always getting entries from the ROOT
 	$tmpAllIOEntries = nc__io__getIOEntries($_SESSION["file"]["open"]);
 
+	//create context menu for general use (create items and get parent folder properties)
+	createContextMenu(
+		//array of context menu elements (id => caption)
+		array(
+			"1" => "create folder",
+			"2" => "create text file",
+			"3" => "create code file",
+			"4" => "",
+			"5" => "properties"
+		),
+		//function name to handle click on menu element
+		'onIoViewModeClick',
+		//container class that where right-click (event:contextmenu) takes place 
+		'.nc-open-file-dialog',
+		//unique id for the context menu
+		'nc_cntx_menu_general',
+		//ignore objects with this classes when handling right-click (event:contextmenu)
+		'.nc-io-entry-format'
+	);
+
+	//create context menu for specific itme
+	createContextMenu(
+		//array of context menu elements (id => caption)
+		array(
+			"6" => "create copy here",
+			"7" => "delete",
+			"8" => "rename",
+			"9" => "",
+			"10" => "properties"
+		),
+		//function name to handle click on menu element
+		'onIoViewModeClick',
+		//container class that where right-click (event:contextmenu) takes place 
+		'.nc-io-entry-format',
+		//unique id for the context menu
+		'nc_cntx_menu_entry_specific',
+		//no ignored class
+		''
+	);
 
 	//create outer div
 	echo '<div style="width: 100%;">';
