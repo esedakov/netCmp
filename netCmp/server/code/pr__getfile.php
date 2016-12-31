6fbac71da07442d3a6c99cbd9d7729445b15496a
esedakov <esedakov@msn.com>
2016-12-31 13:53:20 -0800

    check if both required parameters (file id and file type) have been passed in, otherwise, abort

--- netCmp/server/code/pr__getfile.php
+++ netCmp/server/code/pr__getfile.php
@@ -7,4 +7,15 @@
 	Dependencies:	(none)
 	*/
 
+	//check if POST has 'file_id' and 'file_type' are passed in
+	if( array_key_exists('f', $_POST) && array_key_exists('t', $_POST) ){
+
+
+	} else {	//else, did not pass file id and type
+
+		//error
+		die("missing required POST attributes");
+
+	}	//end if POST has 'file_id' and 'file_type' passed in
+
 ?>
