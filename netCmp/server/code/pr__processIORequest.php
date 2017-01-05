1ef87e2a74d0dd1b8142e7460d592775a5460a9e
esedakov <esedakov@msn.com>
2017-01-04 16:53:16 -0800

    include libraries for IO, db, and utils. also create/retrieve session

--- netCmp/server/code/pr__processIORequest.php
+++ netCmp/server/code/pr__processIORequest.php
@@ -10,6 +10,18 @@
 	//if method is passed in
 	if( array_key_exists('method', $_POST) ){
 
+		//include library for functions 'nc__util__reInitSession', 'nc__util__isIOEntryNameValid'
+		require_once './lib/lib__utils.php';
+
+		//include DB library for function 'nc__db__createIORecord'
+		require_once './lib/lib__db.php';
+
+		//include IO library for function 'nc__io__create'
+		require_once './lib/lib__io.php';
+
+		//re-initialize session
+		nc__util__reInitSession();
+
 
 	}	//end if method is passed in
 
