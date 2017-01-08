266febeeb13c1f1a338b1c2b8a57f69ffbd207db
esedakov <esedakov@msn.com>
2017-01-08 06:39:05 -0800

    create function 'nc__dbg__printHeaderTblRow' that is solely used by 'nc__dbg__printDump' function. itself it creates a header table row with field keys organized as table columns

--- netCmp/server/code/lib/lib__dbg.php
+++ netCmp/server/code/lib/lib__dbg.php
@@ -7,4 +7,14 @@
 	Dependencies:	(none)
 	*/
 
+	//print header row for dump table (used solely by 'nc__dbg__printDump')
+	//input(s):
+	//	row: (php set) any row of the table to display
+	//	doPrintKeys: (boolean) should print keys or values
+	//output(s): (none)
+	function nc__dbg__printHeaderTblRow($row, $doPrintKeys){
+
+
+	}	//end function 'nc__dbg__printHeaderTblRow'
+
 ?>
