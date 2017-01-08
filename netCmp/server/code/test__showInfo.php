4632a79775bc402ceb7bb7eeef74a91adc705f77
esedakov <esedakov@msn.com>
2017-01-08 07:00:11 -0800

    revise debug file to simply show tabular view of session struct

--- netCmp/server/code/test__showInfo.php
+++ netCmp/server/code/test__showInfo.php
@@ -1,9 +1,12 @@
 <?php
 	//include library for function 'nc__util__reInitSession'
 	require_once './lib/lib__utils.php';
+	//include library for debugging functions
+	require_once './lib/lib__dbg.php';
 	//re-initialize session
 	nc__util__reInitSession();
-	var_dump($_SESSION);
-	echo("\n=================================\n");
+	//var_dump($_SESSION);
+	//echo("\n=================================\n");
 	//var_dump($GLOBALS);
+	nc__dbg__printDump($_SESSION, true);
 ?>
