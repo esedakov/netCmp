05d74d203942e9fc6254959fcf8db5e3676768d2
esedakov <esedakov@msn.com>
2017-01-21 09:23:44 -0800

    updated comments

--- netCmp/server/code/pr__processIORequest.php
+++ netCmp/server/code/pr__processIORequest.php
@@ -4,7 +4,7 @@
 	Date:			2016-12-31
 	Description:	process IO request
 	Used by:		(AJAX:openFileDialog.php)
-	Dependencies:	(lib__utils.php)
+	Dependencies:	(utils), (db), (io), (fattr)
 	*/
 
 	//if method is passed in
