1ac6481fd753da3765808ced26cef7c91df94df4
esedakov <esedakov@msn.com>
2017-01-24 07:14:22 -0800

    same as previous commit, with an exception that the code that got moved now, is close to the top of vw__openFileDialog.php. But the reason for moving is the same, i.e. to avoid collissions.

---
+++ netCmp/server/code/pre__openFileDialog.php
@@ -0,0 +1,44 @@
+<?php
+
+	//create context menu for general use (create items and get parent folder properties)
+	createContextMenu(
+		//array of context menu elements (id => caption)
+		array(
+			"1" => "create folder",
+			"2" => "create text file",
+			"3" => "create code file",
+			"4" => "upload image from client",
+			"" => "",
+			"5" => "properties"
+		),
+		//function name to handle click on menu element
+		'onIoViewModeClick',
+		//container class that where right-click (event:contextmenu) takes place 
+		'.nc-open-save-file-dialog',
+		//unique id for the context menu
+		'nc_cntx_menu_general',
+		//ignore objects with this classes when handling right-click (event:contextmenu)
+		'.nc-io-entry-format'
+	);
+
+	//create context menu for specific itme
+	createContextMenu(
+		//array of context menu elements (id => caption)
+		array(
+			"6" => "create copy here",
+			"7" => "delete",
+			"8" => "rename",
+			"9" => "",
+			"10" => "properties"
+		),
+		//function name to handle click on menu element
+		'onIoViewModeClick',
+		//container class that where right-click (event:contextmenu) takes place 
+		'.nc-io-entry-format',
+		//unique id for the context menu
+		'nc_cntx_menu_entry_specific',
+		//no ignored class
+		''
+	);
+
+?>
