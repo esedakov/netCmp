167fa5093e68d35e3af4566e3676798af04a5163
esedakov <esedakov@msn.com>
2017-01-24 08:01:52 -0800

    create function for processing debugging command

--- netCmp/server/code/vw__toolbar.php
+++ netCmp/server/code/vw__toolbar.php
@@ -87,6 +87,137 @@ echo <<<"__EOT_1"
 		}	//end switch -- depenging on view string identifier
 	
 	};	//end function 'ncToolBarShowView'
+
+	//derived from keypress-handler in dbg.js
+	//exec command: start(1), stop(2), restart(3), pause(4), next(5), step in(6)
+	//input(s):
+	//	val: (integer) type of invoked command
+	//output(s): (none)
+	function ncToolBarCommand(val){
+
+		//get debugger (do not need to pass any values)
+		var tmpDbg = dbg.getDebugger();
+
+		//depending on the type of command
+		switch(val){
+
+			//start
+			case 1:
+
+				//if started
+				if( g_started ){
+
+					//set debugger mode to be run non-stop
+					tmpDbg.getDFS()._mode = DBG_MODE.NON_STOP;
+
+				} else {	//else, has not started yet
+
+					//start debugger
+					//do not RUN, it will be done after this switch
+					//g_int.run(g_int._curFrame);
+
+					//confirm that debugger started
+					g_started = true;
+
+				}	//end if started
+
+				break;
+
+			//stop
+			case 2:
+			
+				//stop debugger
+				tmpDbg.quitDebugger();
+
+				break;
+
+			//restart
+			case 3:
+
+				//reset static and non-static fields, the current frame,
+				//	and load variables
+				entity.__interp.restart();
+
+				break;
+
+			//pause
+			case 4:
+
+				//try to pause by setting debugging mode to step in
+				tmpDbg.getDFS()._mode = DBG_MODE.STEP_IN;
+
+				break;
+
+			//next
+			case 5:
+
+				//set debugger to be in STEP_OVER mode
+				tmpDbg.getDFS()._mode = DBG_MODE.STEP_OVER;
+
+				break;
+
+			//step in
+			case 6:
+
+				//set debugger to be in STEP_IN mode
+				tmpDbg.getDFS()._mode = DBG_MODE.STEP_IN;
+
+				break;
+
+		}	//end switch -- depending on the type of command
+
+		//declare var for returned value from RUN function
+		var tmpRunVal;
+
+		//if returning function value from stepped in function
+		if( tmpDbg.getDFS()._val != 0 ){
+		
+			//invoke run and pass in return value
+			entity.__interp.run(tmpDbg.getDFS()._frame, tmpDbg.getDFS()._val);
+		
+			//reset return value to 0
+			tmpDbg.getDFS()._val = 0;
+		
+		} else {	//regular execution
+		
+			//invoke interpreter's run function
+			tmpRunVal = entity.__interp.run(tmpDbg.getDFS()._frame);
+		
+		}
+		
+		//if return value from RUN function is defined and NULL
+		if( 
+			//make sure that RUN returned smth
+			typeof tmpRunVal != "undefined" &&
+			
+			//make sure RUN function quit
+			tmpRunVal == null && 
+			
+			//call stack is not empty
+			tmpDbg._callStack.length > 0 &&
+
+			//make sure it is not EXIT command
+			tmpDbg._callStack[tmpDbg._callStack.length - 1]._funcCall != null
+		){
+
+			//pop last entry from call stack
+			var tmpLstCallStk = tmpDbg._callStack.pop();
+
+			//get functinoid id for the completed function call
+			var tmpFuncId = tmpLstCallStk._funcCall._funcRef._id;
+			
+			//get function call object
+			var tmpFuncCallObj = tmpLstCallStk._frame._funcsToFuncCalls[tmpFuncId];
+			
+			//get return value from completed function call
+			tmpDbg.getDFS()._val = tmpFuncCallObj._returnVal;
+			
+			//re-draw cursor
+			tmpDbg.showCursor();
+		
+		}	//end if return value from RUN function is defined and NULL
+
+	};	//end function 'ncToolBarCommand'
 </script>
 <div class="nc-toolbar-column row bs-glyphicons" style="height:85%; width:100%;">
 	<div class="col-xs-1 col-md-1" style="height:100%;">
