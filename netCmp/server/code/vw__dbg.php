20c48d438cd89314db37aa5c20c128e6147cefcc
esedakov <esedakov@msn.com>
2017-01-24 08:02:42 -0800

    create function for assembling string of all files to be parsed, then parse it and setup interpreter for it

--- netCmp/server/code/vw__dbg.php
+++ netCmp/server/code/vw__dbg.php
@@ -23,7 +23,7 @@
 		style="width: 100%; height: 100%; overflow: scroll; position: relative;">
 	</div>
 
-</div></div>
+</div>
 
 <script type="text/javascript">
 	
@@ -39,6 +39,274 @@
 
 	<?php //store id of DIV where to show CFG ?>
 	var g_dbg_id = 'dbg_holder';
+
+	<?php
+		//compile project
+		//input(s):
+		//	id: (integer) project folder id
+		//output(s): (none)
+	?>
+	function nc__dbg__cmp(id){
+
+		<?php //get name of the file that is currently opened ?>
+		var tmpCurFileName = 
+			$(".nav-tabs > li[role='presentation'][class='active']")
+			.find("a")
+			.text();
+
+		<?php //if file is inside g_files ?>
+		if( tmpCurFileName in g_files ){
+
+			<?php //save file inside g_file ?>
+			g_files[tmpCurFileName] = {
+				code: g_code,
+				line: g_curLineNum,
+				letter: g_curLetterNum,
+				tabs: g_tabs
+			};
+
+		}	<?php //end if file is inside g_file ?>
+
+		<?php //get ids of files that are opened in code view ?>
+		var tmpFileInfo = $(".nc-codeview-win")
+			
+			<?php //get all code view tabs ?>
+			.find("li[role='presentation'] > a")
+
+			<?php //get file ids 
+				//see: http://stackoverflow.com/a/2754033
+			?>
+			.map(function(){
+
+				<?php //get file id ?>
+				var t = $(this).attr("f");
+
+				<?php //ignore tabs where no file associated ?>
+				if(typeof t == "undefined"){
+
+					<?php //return empty ?>
+					return [];
+
+				}
+
+				<?php //return combination of id and caption ?>
+				return t+"/"+$(this).html();
+
+			})
+			.get();
+
+		<?php //init associate array for id => caption ?>
+		var tmpIdToCapArr = [];
+
+		<?php //loop thru received combinations ?>
+		for(var tmpComb in tmpFileInfo){
+
+			<?php //split by delimeter '/', i.e. ID/CAP ?>
+			var tmpArr = tmpComb.split('/');
+
+			<?php //add new entry in associative array ?>
+			tmpIdToCapArr[tmpArr[0]] = tmpArr[1];
+
+		}	<?php //end loop thru received combinations ?>
+
+		<?php //get all file ids for the project via ajax call ?>
+		$.ajax({
+			url: 'pr__getprojectfiles.php',
+			method: 'POST',
+
+			data: {
+
+				<?php //only id for the selected project folder ?>
+				'f':id,
+
+				<?php //get only ids of files that belong to project ?>
+				'i': true
+			
+			}
+
+		}).done(function(data){
+
+			<?php //split received data into array of file ids ?>
+			var tmpNeedToLoadFileIds = data.split(",");
+
+			<?php //associative array of file ids => file name that
+					//are in this project and loaded in code view ?>
+			var tmpCodeViewFileIdToCapArr = [];
+
+			<?php //identify those files that are not opened in the
+				//codeview, and load them from the server ?>
+			for( var tmpFileId in Object.keys(tmpIdToCapArr) ){
+
+				<?php //determine index of file id inside array
+						//of files that need to be loaded ?>
+				var tmpIdx = tmpNeedToLoadFileIds.indexOf(tmpFileId);
+
+				<?php //if file is already in code view ?>
+				if( tmpIdx != -1 ){
+
+					<?php //do not load this file, del it from array ?>
+					tmpNeedToLoadFileIds.splice(tmpIdx, 1);
+
+					<?php //add this file to array ?>
+					tmpCodeViewFileIdToCapArr[tmpIdx] = tmpIdToCapArr[tmpIdx];
+
+				}	<?php //end if file is already in code view ?>
+
+			}	<?php //end loop thru array of file ids ?>
+
+			<?php //load files that are not in codeview ?>
+			$.ajax({
+				url: 'pr__getprojectfiles.php',
+				method: 'POST',
+
+				data: {
+
+					<?php //project folder id ?>
+					'f': id,
+
+					<?php //specify list of requested file ids ?>
+					'l': tmpNeedToLoadFileIds.join(","),
+
+					<?php //get file contents not ids ?>
+					'i': false
+				}
+			}).done(function(data){
+
+				<?php //loop thru project files that in code view ?>
+				for( var tmpFileId in Object.keys(tmpCodeViewFileIdToCapArr) ){
+
+					<?php //get file name ?>
+					var tmpFileName = tmpCodeViewFileIdToCapArr[tmpFileId];
+
+					<?php //if file is in g_files ?>
+					if( tmpName in g_files ){
+
+						<?php //if file is empty or is image ?>
+						if( g_files[tmpName].code.length == 0 ){
+
+							<?php //skip this file ?>
+							continue;
+
+						}	<?php //end if file is empty or image ?>
+
+						<?php //add file content ?>
+						data += "\r\n" + g_files[tmpName].code;
+
+					}	<?php //end if file is in g_files ?>
+
+				}	<?php //end loop thru codevirew project files ?>
+
+				<?php //init var for code string free from comments ?>
+				var tmpData = "";
+
+				<?php //flag: has multiline comment began ?>
+				var tmpIsMLineCom = false;
+
+				<?php //remove all comments ?>
+				$.each(
+
+					<?php //split code by newline characters ?>
+					data.split('\n'), 
+					
+					<?php //iterate thru each line ?>
+					function(key,val){
+
+						<?php //add EOF ?>
+						val += "\n";
+
+						<?php //find index of single line comment start(if any) ?>
+						var m = val.indexOf("//");
+
+						<?php //remove single line comments ?>
+						var t = val.substring(
+
+							<?php //substring starting from beginning ?>
+							0,
+
+							<?php //till the start of comment '//'
+									//or till the end of line ?>
+							( m < 0 ? val.length : m )
+
+						);
+
+						<?php //if ending of multiline comment ?>
+						if( t.indexOf('*/') != -1 ){
+
+							<?php //de-assert flag for m-comment ?>
+							tmpIsMLineCom = false;
+
+							<?php //if line has '/*' ?>
+							if( t.indexOf('/*') != -1 ){
+
+								<?php //try to remove /*...*/ comments ?>
+								t = t.replace(/\/\*.*\*\//,"");
+
+							} else {	<?php //else, line has no '/*' ?>
+
+								<?php //remove first half till '*/' ?>
+								t = t.substring(
+
+									<?php //after '*/' till end ?>
+									t.indexOf("*/") + 2
+
+								);
+
+							}	<?php //end if line has '/*' ?>
+
+						}	<?php //end if ending multiline comment ?>
+
+						<?php //if line is inside multiline comment ?>
+						if( tmpIsMLineCom ){
+
+							<?php //skip line ?>
+							tmpData += "";
+
+							<?php //quit this iteration ?>
+							return;
+
+						}	<?php //end if line is inside m-comment ?>
+
+						<?php //if starting of multiline comment ?>
+						if( t.indexOf('/*') != -1 ){
+
+							//assert flag for multiline comment
+							tmpIsMLineCom = true;
+
+							<?php //try to remove /*...*/ comments ?>
+							t = t.replace(/\/\*.*[\*\/|\n]/,"");
+
+						}	<?php //end if starting multiline comment ?>
+
+						<?php //if string does not contain new line AND
+								//it is not empty ?>
+						if( t.indexOf('\n') == -1 && 
+							$.trim(t).length > 0 
+						){
+
+							<?php //append at the end of line ?>
+							t += "\n";
+
+						}	<?php //end if string has no new line ?>
+
+						<?php //store resulting line ?>
+						tmpData += t;
+					}
+				);
+
+				<?php //create new instance of parser and feed
+						//with the code string ?>
+				g_int = new interpreter(
+					tmpData, 
+					g_dbg_w, 
+					g_dbg_h, 
+					g_dbg_id
+				);
+
+			});	<?php //end AJAX -- done function ?>
+
+		});	<?php //end AJAX -- done function ?>
+
+	};	<?php //end function 'nc__dbg__cmp' ?>
 
 	//invoked from the codeview 'enterkey' event handler to process
 	//	'^O'  key combination to choose which project to compile
