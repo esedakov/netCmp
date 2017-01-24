0672feaa1068135997337fcc35f842838fb232fc
esedakov <esedakov@msn.com>
2017-01-24 07:44:37 -0800

    create main html interface for debugging view, which is comprised from the VIEWPORT div and DBG_HOLDER where CFG will be drawn

--- netCmp/server/code/vw__dbg.php
+++ netCmp/server/code/vw__dbg.php
@@ -11,3 +11,16 @@
 	require_once './js__codeview.php';
 
 ?>
+
+<div 
+	id="dbg_viewport" 
+	class="nc-dbg-win nc-component-view" 
+	style="height:100%; display: none;"
+>
+
+	<div
+		id="dbg_holder"
+		style="width: 100%; height: 100%; overflow: scroll; position: relative;">
+	</div>
+
+</div>
