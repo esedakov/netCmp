31573197b4be3f891616c70d9bdafc0be5daacf7
esedakov <esedakov@msn.com>
2017-02-06 02:45:17 -0800

    create file for tracking user traffic

---
+++ netCmp/server/code/analyticstracking.php
@@ -0,0 +1,17 @@
+<?php
+	//author: Eduard Sedakov
+	//date: 2017-01-30
+	//purpose: for tracking website visits
+?>
+
+<?php //taken from url: https://analytics.google.com/analytics/web/?authuser=0#management/Settings/a91102035w135071563p139206195/%3Fm.page%3DTrackingCode%26_r.ghFlowId%3D6324039/ ?>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-91102035-1', 'auto');
+	ga('send', 'pageview');
+
+</script>
