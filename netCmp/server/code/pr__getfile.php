4e9054d5c1348fa4a8024dc697361be7524ae760
Eduard Sedakov <esedakov@msn.com>
2017-02-06 23:13:46 +0000

    design separate flow code flow for retriving file given name and path

--- netCmp/server/code/pr__getfile.php
+++ netCmp/server/code/pr__getfile.php
@@ -7,9 +7,25 @@
 	Dependencies:	(none)
 	*/
 
+	//include library for function 'nc__util__reInitSession'
+	require_once './lib/lib__utils.php';
+
+	//include library for function 'nc__db__getIOEntryAttrs' and 'nc__db__getFileLocation'
+	require_once './lib/lib__db.php';
+
+	//include library for permission constants
+	require_once './lib/lib__fperm.php';
+
+	//include library for function 'nc__io__getIOEntries'
+	require_once './lib/lib__io.php';
+
+	//re-initialize session
+	nc__util__reInitSession();
+
 	//check if POST has 'file_id' and 'file_type' are passed in
 	if( array_key_exists('f', $_POST) && array_key_exists('t', $_POST) ){
 
+		/* ES 2017-02-05 (b_bug_fix_file): move block of code outside of IF statement to be able to use for next IF
 		//include library for function 'nc__util__reInitSession'
 		require_once './lib/lib__utils.php';
 
@@ -24,6 +40,7 @@
 
 		//re-initialize session
 		nc__util__reInitSession();
+		ES 2017-02-05 (b_bug_fix_file): end move block of code */
 
 		//get type of IO item
 		$itemType = $_POST["t"];
@@ -62,7 +79,9 @@
 				if( $itemType == "2" ) {
 
 					//send back url for the image
-					echo "http://".$_SERVER["SERVER_NAME"].":8080/public_folder/".$loc->_name;
+					//ES 2017-02-05 (b_big_fix_file): remove unneeded port number and change
+					//	public folder to 'pub' which reflects the actual folder name
+					echo "http://".$_SERVER["SERVER_NAME"]."/pub/".$loc->_name;
 
 				} else {	//else, if text/code file
 
@@ -107,8 +126,40 @@
 	} else {	//else, did not pass file id and type
 
 		//error
-		die("missing required POST attributes");
+		//die("missing required POST attributes");
 
 	}	//end if POST has 'file_id' and 'file_type' passed in
+
+	//ES 2011-02-05 (b_bug_fix_file): check if POST has 'file_path' and 'file_name' are passed in
+	if( array_key_exists('p', $_POST) && array_key_exists('n', $_POST) ){
+
+		//get full file info:{name, type, id}
+		$tmpFileInfo = nc__db__getFileByPathAndName($_POST['n'], $_POST['p']);
+
+		//if file was not found
+		if( count($tmpFileInfo) == 0 ){
+
+			//error
+			die("file cannot be found");
+
+		}	//end if file was not found
+
+		//if this is an image
+		if( $tmpFileInfo['type'] == "2" ){
+
+			//set image header
+			//      see: http://stackoverflow.com/a/1851856
+			header('Content-Type: image/jpeg');
+			header('Content-Length: ' . filesize($tmpFileInfo['name']));			
+
+		}	//end if this is an image
+
+		//output image file in 64-bit data format
+		readfile($tmpFileInfo['name']);
+
+		//abort now
+		exit;
+
+	}	//ES 2017-02-05 (b_bug_fix_file): end if 'file_path' and 'file_name' are passed in
 
 ?>
