6bad9a490bf296d46e132d9fee4266708ca83d06
esedakov <esedakov@msn.com>
2017-11-12 10:20:22 +0300

    create ctor for canvasElement that is used to store positon, dimensions, and other relevant information about drawn object on canvas (in alternative drawing approach, jointjs was storing it inside SVG framework, itself)

--- netCmp/parsing/util__canvasElement.js
+++ netCmp/parsing/util__canvasElement.js
@@ -19,3 +19,32 @@ canvasElement.__nextId = 1;
 canvasElement.reset = function() {
 	canvasElement.__nextId = 1;
 };	//end function 'reset'
+
+//class Canvas Element declaration:
+//This class represent CFG object (scope, block, command, symbol), when it is
+//	drawn inside Canvas environment. It is solely used for Canvas, since SVG
+//	approach (which is used inside JointJS) has its own internal data structures
+//	that keep track of each object's position.
+//input(s):
+//	x: (number) x-coordinate of top-left corner
+//	y: (number) y-coordinate of top-left corner
+//	width: (number) width of axis-aligned bounding box
+//	height: (number) height of axis-aligned bounding box
+//	type: (RES_ENT_TYPE) object's type: scope, block, command, symbol
+//	obj: (js Object) associated object
+//	symbList: (string) comma-separated string of symbols that is associated with this object.
+//						It is essentially used to describe symbols attached to commands.
+//output(s): (none)
+function canvasElement(x, y, width, height, type, obj, symbList) {
+	//id
+	this._id = canvasElement.__nextId++;
+	//assign fields
+	this._x = x;
+	this._y = y;
+	this._width = width;
+	this._height = height;
+	this._type = type;
+	this._obj = obj;
+	//split comma-separated list into array of symbols
+	this._symbArr = symbList.split(',');
+};	//end ctor
