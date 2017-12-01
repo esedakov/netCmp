6b68d19e5f944144f631bcd52a10d23ca7e9211d
esedakov <esedakov@msn.com>
2017-12-01 23:04:31 +0300

    list of transformation types: translate, rotate, and remove

---
+++ netCmp/parsing/util__canvasTransformOps.js
@@ -0,0 +1,13 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2017-11-30
+	Description:	transform operations on canvas objects
+	Used by:	(canvas map)
+	Dependencies:	(none)
+**/
+
+var CANVAS_TRANSFORM_OPS_TYPE = {
+	TRANSLATE: {value: 1, name: "translate"},
+	ROTATE: {value: 2, name: "rotate"},
+	REMOVE: {value: 3, name: "remove"}
+};
