03d326ec1ac846153c4a49c6bef6e406676f2ac4
esedakov <esedakov@msn.com>
2017-12-14 23:42:57 +0300

    set file description

---
+++ netCmp/lib/Rectangle/rect.js
@@ -0,0 +1,7 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2017-12-14
+	Description:	library for rectangle type
+	Used by: {interpreter}
+	Depends on:	{interpeter}, content
+**/
---
+++ netCmp/parsing/obj__rectType.js
@@ -0,0 +1,7 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2017-12-14
+	Description:	rectangle type
+	Used by:	{drawing}, {quad-tree}, {math}
+	Dependencies: {Point}
+**/
