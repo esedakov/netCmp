5ba7c1fd9dd9377d2665274efa6f29fe9e07ca13
esedakov <esedakov@msn.com>
2017-12-23 07:38:05 -0800

    create global vars for cell: unique id and library to keep track of all created cells

--- netCmp/parsing/util__grid_cell.js
+++ netCmp/parsing/util__grid_cell.js
@@ -5,3 +5,13 @@
 	Used by: util__grid.js
 	Depends on:	(none)
 **/
+
+//==========globals:==========
+
+//unique identifier assigned to each grid cell
+GridCell.__nextId;
+
+//store created grid cell instances in library indexed by their respective id
+//	key: unique grid cell id
+//	value: grid cell instance
+GridCell.__library;
