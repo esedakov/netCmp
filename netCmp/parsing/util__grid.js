76bf6b5d590f734de114e0b2aedd2fd6f12ecbc7
esedakov <esedakov@msn.com>
2017-12-23 07:37:31 -0800

    create description for grid and cell files

---
+++ netCmp/parsing/util__grid.js
@@ -0,0 +1,7 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2017-12-21
+	Description:	grid that is used for organizing canvas elements stored in canvas map
+	Used by: (viz)
+	Depends on:	util__grid_cell.js
+**/
---
+++ netCmp/parsing/util__grid_cell.js
@@ -0,0 +1,7 @@
+/**
+	Developer:	Eduard Sedakov
+	Date:		2017-12-21
+	Description:	grid cell
+	Used by: util__grid.js
+	Depends on:	(none)
+**/
