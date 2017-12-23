4fea0067abf6582df80698b508bf0cb28c0a8143
esedakov <esedakov@msn.com>
2017-12-23 07:44:53 -0800

    create grid class ctor that initializes following data fields: - cells = hashing-collection of cells, indexed by their address string - count = number of non-empty cells (Note: only non-empty cells are stored inside grid) - objects = hashing-collection of objects stored in any grid cell, indexed by their object index (RES_ENTITY_TYPE+ID) - width, height = dimensions of grid, that make up bounding box around non-empty cells of grid

--- netCmp/parsing/util__grid.js
+++ netCmp/parsing/util__grid.js
@@ -22,3 +22,36 @@ Grid.reset = function() {
 
 //initialize grid global parameter(s)
 Grid.reset();
+
+//class Grid declaration:
+//grid is as of now used solely for more optimized way of organizing and storing
+//	canvas elements on the canvas map inside application view.
+//input(s): (none)
+//output(s): (none)
+function Grid() {
+	//initialize empty list of grid cells
+	//	Note 1: storing only non-empty cells (initially no cells at all)
+	//	Note 2: this is an associative array, where key is location string of cell, of
+	//		the following format: "x1y45" to represent cell on row 45 and column 1
+	//	Note 3: column and row indexes start at 0, so first grid cell has address "x0y0"
+	//	Note 4: all grid cells are of the same size, and they are squares, i.e. width=height
+	this._cells = {};
+	//counter for non-empty grid cells
+	this._count = 0;
+	//collect all objects that are currently stored in grid
+	//	Note 1: this is also associative array, but a reverse of '_cells'
+	//	Note 2: key is RES_ENT_TYPE.name+id (e.g. COMMAND31) and value is also associative
+	//		array with two fields: 'obj' => this object stored in grid, and 'cell' is an array
+	//		of 2 points ('top-left' and 'bottom-right') cell addresses that make up bounding
+	//		box for the region of cells that contain this object
+	//	Note 3: this is an example record:
+	//		_objects[COMMAND123] = {
+	//			"obj": <command id=123 object reference>,
+	//			"cell": ['x0y0', 'x0y5']
+	//		}
+	this._objects = {};
+	//width of grid in number of cells
+	this._width = 0;
+	//height of grid in number of cells
+	this._height = 0;
+};	//end constructor for Grid
